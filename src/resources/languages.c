df3ec46ff30c3f081fc3a00fff5a0aad08cb3ca5
Magnus Larsen <golgothasTerror101@gmail.com>
2024-10-25 20:51:39 -0700

    Remove OMFv2.1's lang strings to fix IDs

--- src/resources/languages.c
+++ src/resources/languages.c
@@ -3,14 +3,12 @@
 #include "formats/language.h"
 #include "resources/pathmanager.h"
 #include "utils/allocator.h"
-#include "utils/array.h"
 #include "utils/log.h"
+#include <string.h>
 
-static array language_strings;
 static sd_language *language;
 
 int lang_init(void) {
-    // Get filename
     const char *filename = pm_get_resource_path(DAT_ENGLISH);
 
     // Load up language file
@@ -20,31 +18,79 @@ int lang_init(void) {
     }
     if(sd_language_load(language, filename)) {
         PERROR("Unable to load language file '%s'!", filename);
-        goto error_1;
+        goto error_0;
     }
 
-    // Load language strings
-    array_create(&language_strings);
-    for(int i = 0; i < language->count; i++) {
-        array_set(&language_strings, i, language->strings[i].data);
+    if(language->count == 1013){
+        // OMF 2.1 added netplay, and 23 new localization strings.
+        int new_ids[]={
+            149,
+            150,
+            172,
+            173,
+            174,
+            175,
+            176,
+            177,
+            178,
+            179,
+            180,
+            181,
+            182,
+            183,
+            184,
+            185,
+            267,
+            269,
+            270,
+            271,
+            284,
+            295,
+            305,
+            -1 // end sentinel
+        };
+        int gapless_count = new_ids[0];
+        int first_id_after_gap = new_ids[0] + 1;
+        for(int *to_remove = new_ids + 1;; to_remove++) {
+            int end = *to_remove;
+            if(end == -1){
+                end = language->count;
+            } else {
+                omf_free(language->strings[*to_remove].data);
+            }
+            int count = end - first_id_after_gap;
+            memmove(&language->strings[gapless_count], &language->strings[first_id_after_gap], count * sizeof(sd_lang_string));
+            gapless_count += count;
+            first_id_after_gap += count + 1;
+            if(*to_remove == -1)
+                break;
+        }
+        memset(&language->strings[gapless_count], 0, language->count - gapless_count);
+        language->count = gapless_count;
+    } else if (language->count != 990) {
+        PERROR("Unable to load language file '%s', unsupported file version!", filename);
+        goto error_0;
     }
 
     INFO("Loaded language file '%s'.", filename);
+
     return 0;
 
-error_1:
-    sd_language_free(language);
 error_0:
+    sd_language_free(language);
     omf_free(language);
     return 1;
 }
 
 void lang_close(void) {
-    array_free(&language_strings);
     sd_language_free(language);
     omf_free(language);
 }
 
 const char *lang_get(unsigned int id) {
-    return (const char *)array_get(&language_strings, id);
+    if(id > language->count) {
+        PERROR("out of range lang id %u!", id);
+        return "!INVALID!";
+    }
+    return language->strings[id].data;
 }
