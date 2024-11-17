94315ca3c489ca09b4184c31bb7c397d72694a3b
Magnus Larsen <golgothasTerror101@gmail.com>
2024-11-17 15:41:45 -0800

    dependencies.cmake

---
+++ cmake-scripts/Dependencies.cmake
@@ -0,0 +1,62 @@
+# temporarily set these? ehh
+set(ORIGINAL_CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH})
+set(CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake-scripts)
+
+if(NOT VCPKG_TOOLCHAIN)
+	message(WARNING "TODO: Dependencies.cmake for non-vcpkg builds")
+	# SDL2, etc..
+endif()
+
+#---------
+# dependencies that use find_package CONFIG search in vcpkg
+#---------
+if(VCPKG_TOOLCHAIN)
+	find_package(SDL2 CONFIG REQUIRED)
+    add_library(openomf::SDL2 INTERFACE IMPORTED)
+    target_link_libraries(openomf::SDL2 INTERFACE "$<IF:$<TARGET_EXISTS:SDL2::SDL2>,SDL2::SDL2,SDL2::SDL2-static>")
+    add_library(openomf::SDL2main INTERFACE IMPORTED)
+    target_link_libraries(openomf::SDL2main INTERFACE SDL2::SDL2main openomf::SDL2)
+
+	find_package(SDL2_mixer CONFIG REQUIRED)
+	add_library(openomf::SDL2_mixer INTERFACE IMPORTED)
+	target_link_libraries(openomf::SDL2_mixer INTERFACE "$<IF:$<TARGET_EXISTS:SDL2_mixer::SDL2_mixer>,SDL2_mixer::SDL2_mixer,SDL2_mixer::SDL2_mixer-static>")
+
+	find_package(libxmp CONFIG REQUIRED)
+	add_library(openomf::xmp INTERFACE IMPORTED)
+	target_link_libraries(openomf::xmp INTERFACE "$<IF:$<TARGET_EXISTS:libxmp::xmp_shared>,libxmp::xmp_shared,libxmp::xmp_static>")
+
+	find_package(Argtable3 CONFIG REQUIRED)
+
+	# TODO: optional deps (miniupnpc and natpmp)
+	# find_package(miniupnpc CONFIG)
+	# find_package(natpmp CONFIG)
+
+	find_package(unofficial-enet CONFIG REQUIRED)
+	add_library(openomf::enet ALIAS unofficial::enet::enet)
+
+	find_package(confuse REQUIRED)
+	add_library(openomf::confuse ALIAS unofficial::libconfuse::libconfuse)
+else()
+	find_package(enet REQUIRED)
+	find_package(confuse REQUIRED)
+endif()
+
+#---------
+# dependencies that unconditionally use cmake module search
+#---------
+find_package(Epoxy REQUIRED)
+# openomf::epoxy is defined in our FindEpoxy.cmake
+
+find_package(PNG REQUIRED)
+add_library(openomf::png INTERFACE IMPORTED)
+target_link_libraries(openomf::png INTERFACE ${PNG_LIBRARIES})
+target_include_directories(openomf::png INTERFACE ${PNG_INCLUDE_DIRS})
+
+
+# If tests are enabled, find CUnit
+if(USE_TESTS)
+    find_package(CUnit REQUIRED)
+endif()
+
+
+set(CMAKE_MODULE_PATH ${ORIGINAL_CMAKE_MODULE_PATH})
--- cmake-scripts/FindEpoxy.cmake
+++ cmake-scripts/FindEpoxy.cmake
@@ -16,8 +16,8 @@ if (EPOXY_INCLUDE_DIR AND EPOXY_LIBRARY)
 endif ()
 
 if (EPOXY_FOUND)
-    add_library(Epoxy::Main UNKNOWN IMPORTED)
-    set_target_properties(Epoxy::Main PROPERTIES
+    add_library(openomf::epoxy UNKNOWN IMPORTED)
+    set_target_properties(openomf::epoxy PROPERTIES
         IMPORTED_LOCATION "${EPOXY_LIBRARY}"
         INTERFACE_INCLUDE_DIRECTORIES "${EPOXY_INCLUDE_DIR}")
     message(STATUS "Found libepoxy: ${EPOXY_LIBRARY}")
